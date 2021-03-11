ccc53d5eb63f57b6bf58db74dbfb7c9b9b828766
Conscire-squad5 <80338565+Conscire-squad5@users.noreply.github.com>
2021-03-11 17:12:24 -0300

    .

--- backend/App/routes/Comentario.js
+++ backend/App/routes/Comentario.js
@@ -5,6 +5,7 @@ const db = require('../config/database.js')
 module.exports = function (app){
     const database=db();
     app.get('/comentarios/retorna', (req, res)=>{
+        res.header("Access-Control-Allow-Origin", "https://conscire-front.herokuapp.com"); 
     
         const sql = "SELECT * FROM  comentarios;";
         database.query(sql, (error, results) =>{
@@ -21,7 +22,7 @@ module.exports = function (app){
         check('sobrenome', 'Sobrenome é obrigatório com pelo menos 3 caracteres').exists().isLength({min:3}),
         check('msg', 'A mensagem precisa ter pelo menos 3 caracteres').exists().isLength({min:3}),
     ], (req, res)=>{
-        
+        res.header("Access-Control-Allow-Origin", "https://conscire-front.herokuapp.com"); 
         const {nome, sobrenome, msg} = req.body; //Desestruturação do corpo da requisiçao em dois elementos que iremos enviar ao bd
         var errors = validationResult(req);
         if(!errors.isEmpty()){
